ad4cfd2d56d080c866dff593d81b96db4588f6df
Berfin <berfin.saricam@gmail.com>
2019-11-23 13:19:19 -0500

    Added posts model

---
+++ database/migrations/2019_11_23_173457_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increment('id');
+            $table->integer('user_id')
+            $table->integer('post_id')
+            $table->string('first_name');
+            $table->dateTime('postDate');
+            $table->dateTime('updateDate');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
