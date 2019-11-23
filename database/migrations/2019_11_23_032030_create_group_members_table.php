2a290e138e33bd91c98762fd5123a96b89d985de
gdzooks <issa4044@gmail.com>
2019-11-22 22:32:36 -0500

    Added models/migrations for group, group members, and group posts.

---
+++ database/migrations/2019_11_23_032030_create_group_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->primary(['userID', 'groupID']); // toUpdate
+            $table->binary('isLeader');
+            $table->timestamp('joinDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_members');
+    }
+}
