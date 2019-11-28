3f12275c2584a3f9858df9c07af009fcfc2f4953
karl <karljchami@gmail.com>
2019-11-27 19:47:00 -0500

    Event members controller

--- app/Http/Controllers/EventMembersController.php
+++ app/Http/Controllers/EventMembersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\_Event_;
 use App\_EventMembers_;
 use Illuminate\Http\Request;
 
@@ -29,48 +30,28 @@ class EventMembersController extends Controller
         return redirect('event_member')->with('event_member', $event_member);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\_EventMembers_  $_EventMembers_
-     * @return \Illuminate\Http\Response
-     */
     public function show(_EventMembers_ $_EventMembers_)
     {
-        //
+        $events = _EventMembers_::with('_Event_')->get();
+
+        return view::make('event_list')->with('_Event_', $events);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\_EventMembers_  $_EventMembers_
-     * @return \Illuminate\Http\Response
-     */
     public function edit(_EventMembers_ $_EventMembers_)
     {
-        //
+        return view::make('event_member');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\_EventMembers_  $_EventMembers_
-     * @return \Illuminate\Http\Response
-     */
+    // Don't think we need it
     public function update(Request $request, _EventMembers_ $_EventMembers_)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\_EventMembers_  $_EventMembers_
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(_EventMembers_ $_EventMembers_)
+    public function destroy(_EventMembers_ $_EventMembers_, Request $request)
     {
-        //
+        $this->validate($request, ['event_id' => 'required']);
+        $events = $_EventMembers_::with('_Event_')->find($request->input('id'));
+        // An event member has a pk made of 2 keys so how do i call that
     }
 }
