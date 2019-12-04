08b5ab3d150fbd540fd57d5a74d52b94dcfa5890
Rez97 <rezhand97@gmail.com>
2019-12-03 23:01:33 -0500

    Worked on the DMController and DMRecipientsController.

--- app/Http/Controllers/DMController.php
+++ app/Http/Controllers/DMController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
+use Auth;
+use Illuminate\Support\Facades\DB;
+#use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use App\dmmessage;
+use App\DMMessage;
+use App\DMRecipients;
 
 class DMController extends Controller
 {
@@ -14,14 +17,21 @@ class DMController extends Controller
      */
     public function index()
     {   
+    
+    # GOAL: Load all the Authenticated user message and the respective recipient response/message to the dm
+    # Return the respective DM View page with both messages
+        
+    # The Recipient chosen depends on the item clicked on the list of recipients
+       $group_id = "1"; # For Testing Purpose 
+
        #getting the authenticated user id 
-      # $user_id = Auth::id();
-       //get all the message written by the authenticated user from the DMMessage model.
-      # $dm = DMMessage::where('id', $user_id)->get();
-       # trying to load the dm view page and sends a list dm_mess
-      # return view('dm', ['dm_message' => $dm]);
-        return view('dm');
-        #return 'INDEX';
+       $user_id = Auth::id();
+
+       //get all the message written by both the authenticated user and the group/recipient from the dm_recipient model.
+       $dm = DB::table('dm_recipients')->where('group_id', $group_id)->where('user_id', $user_id)->pluck('message_id');
+
+       # trying to load the dm view page and sends a list of messages made by both user and recipient.
+        return view('dm', ['dm' => $dm]);
     }
 
     /**
@@ -42,12 +52,16 @@ class DMController extends Controller
      */
     public function store(Request $request)
     {
-        $dm = new DMMessage();
+        $dm = new DMMessage;
+
+        # the message being sent to the recipient.
         $dm->message_body = $request->input('message_body');
-        #getting the user that created the message
+
+        #getting the id of the authenticated user that created the message
         $request->user()->messages()->save($dm);
+
         #redirect to the dm page
-        return redirect()->route('dm');
+        return redirect('dm')->with('dm', $dm);
     }
 
     /**
@@ -58,7 +72,8 @@ class DMController extends Controller
      */
     public function show($id)
     {
-        #load the dm page get the specific dm message
+        #GOAL: load the Dm page get the specific dm message
+
         return view('dm', ['dm' => DMMessage::findOrFail($id)]);
     }
 
--- app/Http/Controllers/DMRecipientController.php
+++ app/Http/Controllers/DMRecipientController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use App\dmrecipients;
+use App\DMRecipients;
 
 class DMRecipientController extends Controller
 {
@@ -14,12 +15,17 @@ class DMRecipientController extends Controller
      */
     public function index()
     {
-        #getting all the authenticates users
-       # $user_id = Auth::id();
-        #get all the dm recipients from the DMRecipient model
-        #$dmrecipients = DMRecipients::where()
-        #open up the dm recipient page and load the dm recipient list according to the user
-        $name = ["Bob", "Jim", "Tim"];
+        ## GOAL: This method is meant to pass the list of friends/group of the authenticated user to the dm_recipient list view page
+       
+        #getting the id of the authenticates users
+         $user_id = Auth::id();
+
+        #get a list of all the the dm recipients/group from the DMRecipient model
+        $dmrecipients = DB::table('dm_recipients')->where('user_id', $user_id)->pluck('group_id');
+
+        #open up the dm_recipient view page and load the recipient list according to the user
+        $name = ["Bob", "Jim", "Tim"]; #test data (for testing purpose). Unable to retrieve data from db tables.
+
         return view('dm_recipients', ['name' => $name]);
     }
 
@@ -41,13 +47,23 @@ class DMRecipientController extends Controller
      */
     public function store(Request $request)
     {
-        $dm_recipient = new DMRecipients();
+        #GOAL: This method is meant to store the data when an user send a dm to a group/friend on the dm view page.
+        $dm_recipient = new DMRecipients;
+
+        # the id of the user that created the message
         $dm_recipient->user_id= $request->input('user_id');
+
+        # the message id of the message being sent to the recipient
         $dm_recipient->message_id= $request->input('message_id');
+
+        # the id of the message being sent to
         $dm_recipient->group_id= $request->input('group_id');
+
         $dm_recipient->save();
-        #redirect to the dm recipient page with dm recipient page.
-        return redirect('dm_recipients')->with('dm_recipient', $dm_recipient);
+
+        #redirect to the dm view page with the dm recipient object created. So that these info can be 
+        # displayed on the dm frontend
+        return redirect('dm')->with('dm_recipient', $dm_recipient);
     }
 
     /**
@@ -58,6 +74,9 @@ class DMRecipientController extends Controller
      */
     public function show($id)
     {
+        # GOAL: meant to return the dm view page that is shared between the authenticated user and group/friend id
+        # the "id" that is being passed is the group id.
+
         return view('dm_recipient', ['dm_recipient' => DMRecipient::findOrFail($id)]);
     }
 
